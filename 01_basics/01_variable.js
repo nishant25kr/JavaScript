296a0697bcd4a45afa46105d3ab749477da303a8
Nishant kumar <nishant927472@gmail.com>
2025-05-20 21:42:04 +0530

    constants

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "abc@gmail.com"
+var accountPass = "12345" 
+
+accountCity = "Bihar"
+
+accountEmail = "new@gmail.com"
+accountPass = "24352435"
+accountCity = "a;fj;a"
+
+let name;
+
+
+// accountId = 2
+
+// console.log(accountId)
+console.table([accountEmail,accountId,accountCity,name]);
