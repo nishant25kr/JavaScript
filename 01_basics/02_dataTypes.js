b0dbf38fb5f0f87a8f1e43860d5f608fb34166da
Nishant kumar <nishant927472@gmail.com>
2025-05-21 20:59:32 +0530

    datatype and conversions

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as a newer version
+
+//console.log(3); console.log(4) //this code is correct but not readable 
+
+let name = "nishant"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof null)
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,31 @@
+// let score = "23";
+// let score = "23adf";
+let score = "Nishant";
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+let scoreInNumber = Number(score)
+
+// console.log(typeof scoreInNumber)
+// console.log(scoreInNumber)
+
+
+
+
+let isLoggedIn = 0;
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+let boolisLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof boolisLoggedIn)
+console.log(boolisLoggedIn)
+
+//NaN=>Not a number
+/*
+1=true
+0=false
+""=false
+"nishant" = true
+*/
